c4e67cc970273fc2bee978d12e422974ff184de7
tid <tid.develop@gmail.com>
2016-10-28 22:23:23 +0900

    Fixed for SPM

--- Package.swift
+++ Package.swift
@@ -1,6 +1,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftClibxml2"
+    name: "SwiftClibxml2",
+    pkgConfig: "libxml-2.0",
+    providers: [
+        .Brew("libxml2"),
+        .Apt("libxml2-dev")
+    ]
 )
-
